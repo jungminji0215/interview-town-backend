182fba302080d608352f073ac15c49c7fc429757
jungminji <wjdalswl0215@gmail.com>
2025-03-04 03:53:24 +0900

    실시간 답변 기능

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import tagsRouter from "./router/tagsRouter.js";
 import questionsRouter from "./router/questionsRouter.js";
 import answersRouter from "./router/answersRouter.js";
 import { db } from "./db/database.js";
+import { Server } from "socket.io";
 
 const app = express();
 app.use(express.json());
@@ -30,4 +31,17 @@ app.use((error, req, res, next) => {
 
 db.getConnection().then((connection) => console.log("DB 연결 성공"));
 
-app.listen(8080);
+const server = app.listen(8080, () => {
+  console.log("서버가 8080번 포트에서 실행 중입니다.");
+});
+
+const socketIO = new Server(server, {
+  cors: { origin: "*" }, // 임시로 모든 도메인 허용
+});
+
+app.locals.io = socketIO;
+
+socketIO.on("connection", (socket) => {
+  console.log("클라이언트 연결:", socket.id);
+  socketIO.emit("answer", "답변입니다~~!!");
+});
--- controller/answersController.js
+++ controller/answersController.js
@@ -8,8 +8,21 @@ export const getAnswersByQuestionId = async (req, res, next) => {
 };
 
 export const addAnswer = async (req, res, next) => {
-  const { questionId, content } = req.body;
+  console.log("실시간 답변 테스트");
 
-  const data = await answerRepository.addAnswer(questionId, content);
-  res.status(200).json(data);
+  try {
+    const { questionId, content } = req.body;
+    // DB에 새 답변 추가
+    const data = await answerRepository.addAnswer(questionId, content);
+
+    console.log("실시간 답변 테스트 data :>> ", data);
+
+    // 답변 등록 후, socket.io를 통해 모든 클라이언트에 새 답변 방송
+    // app.locals.io는 메인 서버 파일에서 설정한 socket.io 인스턴스
+    req.app.locals.io.emit("newAnswer", data.data.answer);
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
 };
